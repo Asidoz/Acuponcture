c163e47c2ce8b502b5e52b79e7f986fa65e080fb
Asidoz <gtilco@gmail.com>
2017-03-15 20:24:01 +0100

    Suppr commentaires

--- controlers/pathologies.php
+++ controlers/pathologies.php
@@ -3,16 +3,8 @@
 $requete = 	"SELECT patho.desc, meridien.nom FROM patho
 			INNER JOIN meridien ON patho.mer = meridien.code
 			ORDER BY meridien.nom ASC";
-			
+
 $listePathos = $bddr->sql($requete);
 
 $smarty->assign("listePathos",$listePathos);
 
-/*$lastMer = "";
-foreach ($listePathos as $key => $value) {
-	if($value["nom"] != $lastMer) {
-		echo "<h3>".$value["nom"]."</h3>";
-		$lastMer = $value["nom"];
-	}
-	echo $value["desc"]."<br />";
-}*/
