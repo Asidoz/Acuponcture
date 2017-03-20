9cd1df2c2429e93517a23747ba4736769372f43d
Asidoz <gtilco@gmail.com>
2017-03-20 15:27:18 +0100

    Class User opérationnelle
    Possible de se connecter avec le compte admin@aasa.fr / admin.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 
 // SMARTY
 require("tpl/Smarty.class.php");
--- models/User.class.php
+++ models/User.class.php
@@ -10,10 +10,10 @@ class User {
 	private $prenom = "";
 
 	function __construct($bddCo, $emailCo="", $pwdCo="") {
-
 		$this->bdd = $bddCo;
 
 		if($emailCo != "" && $pwdCo != "") {
+
 			$resultCo = $this->bdd->sql("SELECT * FROM users WHERE email='".$emailCo."' AND password='".$this->hashPwd($emailCo,$pwdCo)."'");
 			if(count($resultCo) == 1) {
 				$userBdd = $resultCo[0];
@@ -21,9 +21,32 @@ class User {
 				$this->email  = $userBdd["email"];
 				$this->nom    = $userBdd["nom"];
 				$this->prenom = $userBdd["prenom"];
+
+				$_SESSION["AASA_id"] = $this->id;
+			}
+
+		} elseif(isset($_SESSION["AASA_id"])) {
+			if(is_numeric($_SESSION["AASA_id"]) AND $_SESSION["AASA_id"] > 0) {
+				$resultCo = $this->bdd->sql("SELECT * FROM users WHERE id=".$_SESSION["AASA_id"]);
+				if(count($resultCo) == 1) {
+					$userBdd = $resultCo[0];
+					$this->id     = $userBdd["id"];
+					$this->email  = $userBdd["email"];
+					$this->nom    = $userBdd["nom"];
+					$this->prenom = $userBdd["prenom"];	
+				} else { $this->disconnect(); }
+			} else {
+				$this->disconnect();
 			}
 		}
+	}
 
+	public function disconnect() {
+		unset($_SESSION["AASA_id"]);
+		$this->id     = -1;
+		$this->email  = "";
+		$this->nom    = "";
+		$this->prenom = "";
 	}
 
 	private function hashPwd($email,$pwd) {
@@ -41,7 +64,15 @@ class User {
 	}
 
 	public function getEmail() {
-		return $this->email;
+		return $this->email; 
+	}
+
+	public function getNom() {
+		return $this->nom;
+	}
+
+	public function getPrenom() {
+		return $this->prenom;
 	}
 
 };
