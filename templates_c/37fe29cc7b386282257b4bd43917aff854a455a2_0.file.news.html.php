8e59fa030838f091b83af5ae105a7cece3e8ef6a
CorentinPRIMA <corentin.prima@live.fr>
2017-03-24 16:53:21 +0100

    Modif css et model

--- templates_c/37fe29cc7b386282257b4bd43917aff854a455a2_0.file.news.html.php
+++ templates_c/37fe29cc7b386282257b4bd43917aff854a455a2_0.file.news.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-03-24 13:48:17
-  from "C:\wamp64\www\Acuponcture\view\news.html" */
+/* Smarty version 3.1.30, created on 2017-03-24 15:45:06
+  from "C:\wamp64\www\AcuponctureOk\Acuponcture\view\news.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_58d523a18cfa05_05758555',
+  'unifunc' => 'content_58d53f021b0df7_61620804',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
-    '389b2976f455d4be19db98082125f364e6fff2df' => 
+    '37fe29cc7b386282257b4bd43917aff854a455a2' => 
     array (
-      0 => 'C:\\wamp64\\www\\Acuponcture\\view\\news.html',
-      1 => 1490363295,
+      0 => 'C:\\wamp64\\www\\AcuponctureOk\\Acuponcture\\view\\news.html',
+      1 => 1490367235,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_58d523a18cfa05_05758555 (Smarty_Internal_Template $_smarty_tpl) {
+function content_58d53f021b0df7_61620804 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->tpl_vars['i'] = new Smarty_Variable(null, $_smarty_tpl->isRenderingCache);$_smarty_tpl->tpl_vars['i']->step = 1;$_smarty_tpl->tpl_vars['i']->total = (int) ceil(($_smarty_tpl->tpl_vars['i']->step > 0 ? 2+1 - (0) : 0-(2)+1)/abs($_smarty_tpl->tpl_vars['i']->step));
 if ($_smarty_tpl->tpl_vars['i']->total > 0) {
 for ($_smarty_tpl->tpl_vars['i']->value = 0, $_smarty_tpl->tpl_vars['i']->iteration = 1;$_smarty_tpl->tpl_vars['i']->iteration <= $_smarty_tpl->tpl_vars['i']->total;$_smarty_tpl->tpl_vars['i']->value += $_smarty_tpl->tpl_vars['i']->step, $_smarty_tpl->tpl_vars['i']->iteration++) {
--- templates_c/7895feca94a3e7b217483bbee7becad8b4479894_0.file.model.html.php
+++ templates_c/7895feca94a3e7b217483bbee7becad8b4479894_0.file.model.html.php
@@ -1,3 +1,30 @@
+<?php
+/* Smarty version 3.1.30, created on 2017-03-24 15:51:25
+  from "C:\wamp64\www\AcuponctureOk\Acuponcture\view\model.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_58d5407dc09718_00827763',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7895feca94a3e7b217483bbee7becad8b4479894' => 
+    array (
+      0 => 'C:\\wamp64\\www\\AcuponctureOk\\Acuponcture\\view\\model.html',
+      1 => 1490370683,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:view/news.html' => 1,
+  ),
+),false)) {
+function content_58d5407dc09718_00827763 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -96,7 +123,10 @@
                                     <div  id ="container" class="block-title">
                                       
                                           <section id="contenu">
-                                        {block name=contenu}No content{/block}
+                                        <?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2580858d5407dbbf5d2_73209056', 'contenu');
+?>
+
                                              </section>
 
                                     </div>
@@ -117,14 +147,27 @@
                                                                <section id="infos">
         <h2>Les infos</h2>
         <!--
-        {foreach from=$listeNews key=i item=n}
+        <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['listeNews']->value, 'n', false, 'i');
+if ($_from !== null) {
+foreach ($_from as $_smarty_tpl->tpl_vars['i']->value => $_smarty_tpl->tpl_vars['n']->value) {
+?>
         <article>
-            <h3>{$n["titreNews"]}</h3>
-            <p>{$n["texteNews"]}</p>
+            <h3><?php echo $_smarty_tpl->tpl_vars['n']->value["titreNews"];?>
+</h3>
+            <p><?php echo $_smarty_tpl->tpl_vars['n']->value["texteNews"];?>
+</p>
         </article>
-        {/foreach}
+        <?php
+}
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
+?>
+
 -->
-        {include file="view/news.html"}
+        <?php $_smarty_tpl->_subTemplateRender("file:view/news.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
+
     </section>
                                     
                                 </div>
@@ -153,3 +196,14 @@
 
     </body>
 </html>
+<?php }
+/* {block 'contenu'} */
+class Block_2580858d5407dbbf5d2_73209056 extends Smarty_Internal_Block
+{
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+No content<?php
+}
+}
+/* {/block 'contenu'} */
+}
