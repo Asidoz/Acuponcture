10794cd6954576a84453a5d8f5468801aa1b2d9a
Asidoz <gtilco@gmail.com>
2017-05-08 23:46:21 +0200

    Modif recherche

--- ajax/templates_c/04cda734eb1b6d9f3a8745fa81a4cc1296fa2cea_0.file.onlyVar.html.php
+++ ajax/templates_c/04cda734eb1b6d9f3a8745fa81a4cc1296fa2cea_0.file.onlyVar.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-05-05 12:41:56
+/* Smarty version 3.1.30, created on 2017-05-08 21:43:51
   from "C:\wamp64\www\Acuponcture\view\onlyVar.html" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_590c7314044135_75724280',
+  'unifunc' => 'content_5910e697ad2847_41845275',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '04cda734eb1b6d9f3a8745fa81a4cc1296fa2cea' => 
     array (
       0 => 'C:\\wamp64\\www\\Acuponcture\\view\\onlyVar.html',
-      1 => 1493988114,
+      1 => 1494278066,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_590c7314044135_75724280 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5910e697ad2847_41845275 (Smarty_Internal_Template $_smarty_tpl) {
 ob_start();
 echo $_smarty_tpl->tpl_vars['var']->value;
 $_prefixVariable1=ob_get_clean();
--- scripts/search.js
+++ scripts/search.js
@@ -4,16 +4,23 @@ function recherche() {
 	
 	var liste = "";
 
-	$.post("../ajax/search.php",
-		{
-			s:input.val()
-		},function(data) {
-			var pathos = data.split(" ; ");
-			for (var i = 0; i < pathos.length-1; i++) {
-				var infos = pathos[i].split(" - ");
-				liste += '<a href="'+infos[2]+'" title="'+infos[1]+'">'+infos[1]+'</a><br />';
+	var nbrLettres = (input.val()).length;
+
+	if(nbrLettres == 0) {
+		results.css("display","none");
+	} else {
+		results.css("display","block");
+		$.post("../ajax/search.php",
+			{
+				s:input.val()
+			},function(data) {
+				var pathos = data.split(" ; ");
+				for (var i = 0; i < pathos.length-1; i++) {
+					var infos = pathos[i].split(" - ");
+					liste += '<a href="'+infos[2]+'" title="'+infos[1]+'">'+infos[1]+'</a><br />';
+				}
+				results.html(liste);
 			}
-			results.html(liste);
-		}
-	);
+		);
+	}
 }
